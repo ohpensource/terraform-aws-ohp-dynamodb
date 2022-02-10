50628460454f55b19240d7fcf37ea8bc1513d7b9
Eduard Bargués <88039083+lucky-edu@users.noreply.github.com>
2022-02-10 14:57:39 +0100

    feat: LANZ-1669 conditionally create backup resources (#7)

--- back_up.tf
+++ back_up.tf
@@ -1,28 +1,35 @@
 resource "aws_backup_plan" "dynamodb_backup_plan" {
-  name = "dynamodb_backup_plan"
+  count = var.enable_point_in_time_recovery ? 1 : 0
 
+  name = var.name
   rule {
     rule_name         = "tf_backup_rule"
-    target_vault_name = aws_backup_vault.backup_vault.name
+    target_vault_name = aws_backup_vault.backup_vault[0].name
     schedule          = "cron(0 12 * * ? *)"
   }
 }
 
 resource "aws_backup_vault" "backup_vault" {
-  name        = "dynamodb_backup_vault"
-  kms_key_arn = aws_kms_key.backup_vault_keys.arn
+  count = var.enable_point_in_time_recovery ? 1 : 0
+
+  name        = var.name
+  kms_key_arn = aws_kms_key.backup_vault_keys[0].arn
 }
 
 resource "aws_kms_key" "backup_vault_keys" {
+  count = var.enable_point_in_time_recovery ? 1 : 0
+
   description             = "KMS keys used to encrypt backup vault"
   deletion_window_in_days = 10
   enable_key_rotation     = true
 }
 
 resource "aws_backup_selection" "dynamodb_backup_selection" {
-  iam_role_arn = aws_iam_role.dynamodb_backup_role.arn
-  name         = "tf_dynamodb_backup_selection"
-  plan_id      = aws_backup_plan.dynamodb_backup_plan.id
+  count = var.enable_point_in_time_recovery ? 1 : 0
+
+  name         = var.name
+  iam_role_arn = aws_iam_role.dynamodb_backup_role[0].arn
+  plan_id      = aws_backup_plan.dynamodb_backup_plan[0].id
 
   resources = [
     aws_dynamodb_table.default[0].arn
@@ -30,7 +37,9 @@ resource "aws_backup_selection" "dynamodb_backup_selection" {
 }
 
 resource "aws_iam_role" "dynamodb_backup_role" {
-  name               = "dynamodb_backup_role"
+  count = var.enable_point_in_time_recovery ? 1 : 0
+
+  name               = "${var.name}-backup"
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -48,6 +57,8 @@ POLICY
 }
 
 resource "aws_iam_role_policy_attachment" "dynamodb_backup_role_attachement" {
+  count = var.enable_point_in_time_recovery ? 1 : 0
+
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
-  role       = aws_iam_role.dynamodb_backup_role.name
+  role       = aws_iam_role.dynamodb_backup_role[0].name
 }
